df96be5312e2d61c697d961cf70d3169bead03e5
Thuyvi Nguyen <vivipnguyen@gmail.com>
2020-04-13 13:38:56 -0700

    added initial components

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import Hero from '../components/Hero';
+import Carousel from '../components/Carousel';
+
 function HomePage(props) {
 
     return(
-        <p>HomePage works!</p>
+        <Hero title={props.title} subTitle={props.subTitle} text={props.text}/>
+        <Carousel />
     );
 
 }
