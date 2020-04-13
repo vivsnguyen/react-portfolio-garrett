ca4290f1ced54439cd26b29f2b1de9613b145503
Thuyvi Nguyen <vivipnguyen@gmail.com>
2020-04-13 15:11:41 -0700

    react-spring install, css styling

--- src/components/CardInfo.js
+++ src/components/CardInfo.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { useSpring, animated } from 'react-spring';
 
 function CardInfo(props) {
 
+    const style = useSpring({opacity: 1, from: {opacity: 0}});
 
     return(
-        <p>Card Info works!</p>
-    )
+        <animated.div className="" style={style}>
+            <p>{props.title}</p>
+            <p>{props.subTitle}</p>
+            <a href={props.link} target="_blank" rel="noopener noreferrer">View</a>
+
+        </animated.div>
+
+    );
 
 }
 
