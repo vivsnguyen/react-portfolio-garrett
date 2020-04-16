439281b3fd7efbdaeb1477aae665dc1adfd2c80d
Thuyvi Nguyen <vivipnguyen@gmail.com>
2020-04-16 14:45:39 -0700

    Updates

---
+++ precache-manifest.4d9307e278e1bc75aab1e79f7a9c0eca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0d80a05082c932a60c0788f8186b0a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c77be2d7552d02c2a5d9",
+    "url": "/static/css/2.66165e39.chunk.css"
+  },
+  {
+    "revision": "ae7efdb16276ab6efe6a",
+    "url": "/static/css/main.75f23303.chunk.css"
+  },
+  {
+    "revision": "c77be2d7552d02c2a5d9",
+    "url": "/static/js/2.bdde25b0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.bdde25b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae7efdb16276ab6efe6a",
+    "url": "/static/js/main.eea452d7.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "bf79b6757027c409ee6c6107ee2074ce",
+    "url": "/static/media/devgrub.bf79b675.png"
+  }
+]);
