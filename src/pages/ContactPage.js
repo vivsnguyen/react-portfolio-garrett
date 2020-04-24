b068a36742e6318339a35d828203d48aefbd8384
Thuyvi Nguyen <vivipnguyen@gmail.com>
2020-04-24 15:30:19 -0700

    edited contact page to have fa icons

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -3,7 +3,8 @@ import Hero from '../components/Hero';
 import Content from '../components/Contents';
 
 // get our fontawesome imports
-import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function ContactPage(props) {
@@ -13,10 +14,14 @@ function ContactPage(props) {
             <Hero title={props.title} />
 
             <Content>
-            <FontAwesomeIcon icon={faHome} />
-            <a href="https://www.linkedin.com/in/thuyvi-nguyen/">LinkedIn</a>
 
-            <p>email : vivipnguyen@gmail.com</p>
+                <FontAwesomeIcon icon={faLinkedin} size="2x"/>        
+                <a href="https://www.linkedin.com/in/thuyvi-nguyen/">LinkedIn</a>
+                <br></br>
+                <p>
+                    <FontAwesomeIcon icon={faEnvelope} size="2x"/>
+                    email : <a href="mailto:vivipnguyen@gmail.com">vivipnguyen@gmail.com</a>
+                </p>
             </Content>
         </div>
     );
