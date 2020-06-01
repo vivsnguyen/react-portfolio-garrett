3401f0bb4a0bfd7861e4f48a75cb3acd270802b6
Thuyvi Nguyen <vivipnguyen@gmail.com>
2020-06-01 13:53:36 -0700

    updating contact page with icons

---
+++ src/components/Social.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Social extends Component {
+    render() {
+            return (
+            <div class="social">
+            <a href="https://github.com/naaficodes" target="_blank"><i class="fab fa-github"></i></a>
+            <a href="https://www.linkedin.com/in/thuyvi-nguyen/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+            <a href="https://medium.com/@iam_naafi" target="_blank"><i class="fab fa-medium-m"></i></a>
+            </div>
+            )
+        }
+    }
+export default Social
